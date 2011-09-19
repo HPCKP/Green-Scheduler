d2368a8bc95c7401bdbd1145af651edf4a70cd7a
Jordi Blasco Pallares <jordi.blasco@xrqtc.org>
2011-09-19 10:55:40 +0000

    redefined the functions connectors

--- bin/greenscheduler.sh
+++ bin/greenscheduler.sh
@@ -2,53 +2,68 @@
 #
 #                        Green Scheduler Daemon core  
 #===============================================================================
-# AUTHOR:                                      Written by Jordi Blasco Pallarès
+# AUTHOR:                                      Written by Jordi Blasco Pallares
 # REPORTING BUGS:                       Report bugs to <jordi.blasco@gmail.com>.
 #   Please report all bugs after reviewing project details located at
 #              http://sourceforge.net/projects/greenscheduler/
 #===============================================================================
 #
-#set -xv
+set -xv
 source /etc/greenscheduler/greenscheduler.conf
 
 if [ -d $WORKDIR ]; then mkdir -p $WORKDIR; fi
 
 
-connector() {
-if [ $BQS = "SGE" ];then
+if [ $BQS = "SGE" ]; then
    export SGE_ROOT=$BQSPATH
    . $SGE_ROOT/default/common/settings.sh
-   hoststats="qhost | gawk 'BEGIN{NL=2}{print $0}' | grep -v HOSTNAME | grep -v global"
-   #queues="qconf -sql"
-   queues="qconf -sql | grep xhpc.q"  # We only use xhpc.q in order to verify and test the performance.
-   qwjobs="qstat -u *, -s p -q "
-   disablenode="qmod -d $1@$2"
-   enablenode="qmod -e $1@$2"
+   hoststats() {
+      qhost | gawk 'BEGIN{NL=2}{print $0}' | grep -v HOSTNAME | grep -v global
+   }
 
-elif [ $BQS = "PBS" ];then
+   queues() {
+      #qconf -sql
+      qconf -sql | grep xhpc.q  # We only use xhpc.q in order to verify and test the performance.
+   }
+   qwjobs() {
+      qstat -u *, -s p -q 
+   }
+   disablenode() {
+     qmod -d $1@$2
+   }
+   enablenode() {
+     qmod -e $1@$2
+   }
 
-elif [ $BQS = "Torque" ];then
 
-elif [ $BQS = "Slurm" ];then
+elif [ $BQS = "PBS" ]; then
+   echo "pending"
 
-elif [ $BQS = "LSF" ];then
+elif [ $BQS = "Torque" ]; then
+   echo "pending"
+
+elif [ $BQS = "Slurm" ]; then
+   echo "pending"
+
+elif [ $BQS = "LSF" ]; then
+   echo "pending"
 
 else
    echo "ALERT : You must define your Batch Queue System in /etc/greenscheduler/greenscheduler.conf"; exit 0
 fi
-}
 
 nodestatus() {
     whost=$(cat /etc/greenscheduler/whost)
-    $($hoststats) > $WORKDIR/nodestatus.dat
+    hoststats > $WORKDIR/nodestatus.dat
     for ahost in $whost
        do 
 	  echo "Skipping $ahost ..."
 	  gawk -v ahost="$ahost" '{if(match ($1,ahost)==0){print $0}}' $WORKDIR/nodestatus.dat >  $WORKDIR/nodestatus.tmp
           cp $WORKDIR/nodestatus.tmp $WORKDIR/nodestatus.dat
        done
-    for host in $(cat $WORKDIR/nodestatus.dat)
+    for host in $(cat $WORKDIR/nodestatus.dat | gawk '{print $1}')
 	do 
+	    echo $host
 	    # Add data like Temperature, uptime, reboots and create arrays
 	done
 }
@@ -83,7 +98,7 @@ queue=$1
 $ToSTOP=$(SDCandidate $queue $EFSLOTS)
 for node in $ToSTOP
     do
-      $disablenode $queue $node
+      disablenode $queue $node
       vrfy=$(qhost -h $node | grep $node | gawk '{if ($4 < 1){print 1}}')
       if $vrfy; then SDnodes $node; fi
     done
@@ -98,7 +113,7 @@ WUCandidate() {
 WUnodes() {
     node=$1
     utime=$(date +%s)
-    case in $ITYPE
+    case $ITYPE in
       lanplus)
 	ipmitool -I lanplus -H ${node}-${IHEX} -U $LOGIN -P $PASSWD chassis bootdev disk
 	ipmitool -I lanplus -H ${node}-${IHEX} -U $LOGIN -P $PASSWD chassis power on 
@@ -132,6 +147,7 @@ WUnodes() {
 
 Enablenodes() {
 queue=$1
+NSLOTS=$2
 ToWakeUp=$(WUCandidate $queue $NSLOTS)
 for node in $ToWakeUp
     do
@@ -144,19 +160,20 @@ for node in $ToWakeUp
 
 
 CheckQueues() {
-    for $q in $queues; do 
+    for q in $1; do 
+	QW=0
 	QW=$($qwjobs $q)
 	if (( $QW < 0 )); then
 	    NSLOTS=$(($FSLOTS-$MFNSLOTS))
 	    # Needed Slots : it will determine how many nodes it will need to wakeup
-	    if (( $NSLOTS > 0 ))
+	    if (( $NSLOTS > 0 )) ; then
 		Enablenodes $q $NSLOTS
 	    fi
 	    
 	elif (( $QW => 0 )); then
 	    # Exedent Free Slots : it will determine how many nodes it will need to stop
 	    EFSLOTS=$(($FSLOTS-$MFNSLOTS))
-	    if (( $EFSLOTS > 0 ))
+	    if (( $EFSLOTS > 0 )) ; then
 		Disablenodes $q $EFSLOTS
 	    fi
 	fi
@@ -171,6 +188,6 @@ CheckQueues() {
 while true
 do
     nodestatus
-    CheckQueues
+    CheckQueues $queues
     sleep $TIMEOUT
 done
